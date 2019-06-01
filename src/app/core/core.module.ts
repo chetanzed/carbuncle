86224da599b2ac5b6f380d64244baa75ddcaa9cc
el-yomi <shota.kishi@el-soul.com>
2019-06-02 06:42:48 +0900

    usually use forms

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { MaterialModule } from './material.module'
 import { FirebaseModule } from './firebase.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MaterialModule, RouterModule, FirebaseModule]
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    FirebaseModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class CoreModule {}
